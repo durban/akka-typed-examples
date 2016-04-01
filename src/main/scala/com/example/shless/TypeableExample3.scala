7f7378979cbdb70615690a25207c70cf57f332fb
Daniel Urban <urban.dani@gmail.com>
2016-04-01 15:43:37 +0200

    One more shapeless example

---
+++ src/main/scala/com/example/shless/TypeableExample3.scala
@@ -0,0 +1,26 @@
+package com.example.shless
+
+import shapeless._
+
+object TypeableExample3 {
+
+  final case class C[A](s: String) {
+    var y: A = _
+  }
+
+  val tt = Typeable[C[Int]]
+
+  val v1: Any = { val r = C[Int]("a"); r.y = 8; r }
+  val v2: Any = { val r = C[Double]("b"); r.y = 4.5; r }
+
+  def main(args: Array[String]): Unit = {
+    tt.cast(v1).foreach { c: C[Int] =>
+      val i: Int = c.y
+      println(i)
+    }
+    tt.cast(v2).foreach { c: C[Int] =>
+      val i: Int = c.y
+      println(i)
+    }
+  }
+}
